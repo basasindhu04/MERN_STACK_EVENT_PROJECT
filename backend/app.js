1b251a71395a85ef72228026a69320a4025e6bc8
basasindhu <basasindhu04@gmail.com>
2025-10-18 16:56:32 +0530

    Cleaned up merge conflict and fixed root route

--- backend/app.js
+++ backend/app.js
@@ -1,40 +1,32 @@
-import express, { json } from "express";
+import express from "express";
 import { dbConnection } from "./database/dbConnection.js";
 import dotenv from "dotenv";
 import messageRouter from "./router/messageRouter.js";
-import cors from "cors"
+import cors from "cors";
 
 const app = express();
 
-dotenv.config({ path: "./config/config.env"});
+dotenv.config({ path: "./config/config.env" });
 
 app.use(
-    cors ({
-    origin:[process.env.FRONTEND_URL],
+  cors({
+    origin: [process.env.FRONTEND_URL],
     methods: ["POST"],
     credentials: true,
-})
-
+  })
 );
 
-<<<<<<< HEAD
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/",(req,res) =>{
-    res.json({
-        success: true
-        message: "Welcome to my website",
-    });
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Welcome to my website",
+  });
 });
 
-=======
-
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
->>>>>>> 77cded13e30f32436255671c0580d59ba862b60c
-app.use("/api/v1/message",messageRouter);
+app.use("/api/v1/message", messageRouter);
 
 dbConnection();
-export default app; 
+export default app;
